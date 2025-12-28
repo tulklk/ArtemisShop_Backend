98e54b745bd7ad4f9dc5f0c32426324f1867b753
Phan Thanh Tu <tulklk32@gmail.com>
2025-12-28 23:51:13 +0700

    fix veryfied email

--- AtermisShop/AtermisShop.Application/obj/Debug/net8.0/AtermisShop.Application.AssemblyInfo.cs
+++ AtermisShop/AtermisShop.Application/obj/Debug/net8.0/AtermisShop.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AtermisShop.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ede698ee280c28e4e097070b9c7fa518cbf7fc10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f20e416da3f8f20c737dc8d56dcdc401d5d6f45f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AtermisShop.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AtermisShop.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
