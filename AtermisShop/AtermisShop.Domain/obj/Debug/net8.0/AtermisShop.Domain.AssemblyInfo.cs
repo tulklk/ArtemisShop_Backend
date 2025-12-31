58f9535d80680fd8de9acedc6b716113899ecda2
Phan Thanh Tu <tulklk32@gmail.com>
2026-01-01 00:55:25 +0700

    fixx

--- AtermisShop/AtermisShop.Domain/obj/Debug/net8.0/AtermisShop.Domain.AssemblyInfo.cs
+++ AtermisShop/AtermisShop.Domain/obj/Debug/net8.0/AtermisShop.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AtermisShop.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26b1511e52e645f4519784fc8ec0aa88a972cc56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d783cfc1599fe87a22604152d43ad80c50c00a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("AtermisShop.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AtermisShop.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
